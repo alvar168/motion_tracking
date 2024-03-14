4e1aaf081e541966504f2dc1f6d7d4b0de4ed760
Hugo Alvarez Valdivia <hugoalval03@gmail.com>
2024-03-14 11:09:00 -0500

    Adding latency test code

---
+++ latency_test.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import requests
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    start_time = time.time()
+    response = requests.get('http://192.168.4.157:5000')
+    end_time = time.time()
+    latency = end_time - start_time
+    return f'Latency: {latency} seconds'
+
+if __name__ == '__main__':
+    app.run()
